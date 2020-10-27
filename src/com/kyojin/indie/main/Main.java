09aef31fc2943e2bf16c4c23bf4c90bc2e0c1b4e
ErickMedinaLavielle <erickmedlav@gmail.com>
2020-10-26 21:11:05 -0600

    Adding Logo.class and Main.class

---
+++ src/com/kyojin/indie/main/Main.java
@@ -0,0 +1,16 @@
+package com.kyojin.indie.main;
+
+import com.kyojin.indie.logo.Logo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Logo logo = new Logo();
+		
+		logo.printLogo();
+
+	}
+
+}
